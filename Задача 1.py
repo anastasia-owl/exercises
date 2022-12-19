ce12071de68f6ac274a544fab665e74a735d95d0
anastasia-owl <120097769+anastasia-owl@users.noreply.github.com>
2022-12-19 15:03:57 +0300

    Add files via upload

---
+++ Задача 1.py
@@ -0,0 +1,16 @@
+
+def brackets(numder: int, numder_left=0, numder_right=0, sepor =''):
+    if numder_left != numder or numder_right != numder:
+        if numder_left < numder:
+            brackets(numder, numder_left + 1, numder_right, sepor + '(')
+        if numder_right < numder_left:
+            brackets(numder, numder_left, numder_right + 1, sepor + ')')
+    elif:
+        print(sepor)
+
+def main():
+    numder = int(input())
+    print(brackets(numder))
+
+if __name__ == "__main__":
+    main()
